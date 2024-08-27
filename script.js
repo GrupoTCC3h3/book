1a333291de2ebe2933ea82061bee08b1148a50bf
grupotcc3h3 <157658093+viniciusN444@users.noreply.github.com>
2024-08-27 19:00:32 -0300

    Ajustes

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const explorarBtn = document.querySelector('.btn-primary');
+    explorarBtn.addEventListener('click', function() {
+        alert('Explorando livros...');
+    });
+});
