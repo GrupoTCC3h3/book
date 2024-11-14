19a20353927deed0cd34e94efcfd09e9ded365c5
lucasfreitas-cps2 <Lucas.freitas@etec.sp.gov.br>
2024-11-13 22:56:31 -0300

    Ajustes.

--- views/lista_mensagens/lista_mensagens.js
+++
@@ -1,8 +0,0 @@
-function showChat() {
-    document.getElementById("inputContainer").style.display = "flex";
-}
-
-  // Função para voltar à página anterior
-  function voltarPaginaAnterior() {
-    window.history.back();
-}
